6e45ac554e7748b1bb819f24e0f7f427ec226b2a
Harshit Grover <codrrharshit@gmail.com>
2024-10-20 00:29:52 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ 
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        
+      },
+      backgroundImage:{
+        'roadimage': "url('images/background-road-share 1.png')",
+        'backgroundimage':"url('images/miguel-a-amutio-QDv-uBc-poY-unsplash 1.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
